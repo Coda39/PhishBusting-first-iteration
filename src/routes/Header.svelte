4339f3cf95d39dfcfb79a967dd0ddb286798b0f0
Coda39 <mateo404521@gmail.com>
2023-06-07 13:22:58 -0500

    working header component

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,123 +1,124 @@
 <script>
-	import { page } from '$app/stores';
+  import { page } from '$app/stores';
+	import { writable } from 'svelte/store';
+
+  let currentPage = writable(''); 
+	let pageDescription =writable('');
+
+  $: {
+    // Get the current page URL pathname from the $page store
+    const currentPath = $page.url.pathname;
+
+    // Update the currentPage variable based on the current path
+    if (currentPath === '/') {
+      currentPage.set('Welcome');
+			pageDescription.set('')
+    } else if (currentPath === '/zipDomains') {
+      currentPage.set('Zip Domains');
+			pageDescription.set('Zip Domains are a malicious trick that can lead to individuals devulging valuable information all because they thought they where downloading a file')
+    } else if (currentPath.startsWith('/WebSkimmers')) {
+      currentPage.set('Web Skimmers');
+			pageDescription.set('Web Skimmers attacking popular ecommerce sites is more common now than ever and can lead to your sensitive information being at risk')
+    } else {
+      currentPage.set('Unknown Page');
+    }
+  }
 </script>
 
 <header>
-
-	<div class = "corner">
-		<p> </p>
-	</div>
-	<nav>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
-		</svg>
-		<ul>
-			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-				<a href="/">Home</a>
-			</li>
-			<li aria-current={$page.url.pathname === '/zipDomains' ? 'page' : undefined}>
-				<a href="/zipDomains">Zip Domains</a>
-			</li>
-			<li aria-current={$page.url.pathname.startsWith('/WebSkimmers') ? 'page' : undefined}>
-				<a href="/WebSkimmers">Web Skimmers</a>
-			</li>
-		</ul>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
-		</svg>
-	</nav>
-
-	<div class = "corner">
-		<p> </p>
+	<div class="header-content">
+		<div class="header-left">
+			<h1 class="page-title">{$currentPage}</h1>
+			<p class="page-description">{$pageDescription}</p>
+		</div>
+		<nav class="header-right">
+			<ul>
+				<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
+					<a href="/">Home</a>
+				</li>
+				<li aria-current={$page.url.pathname === '/zipDomains' ? 'page' : undefined}>
+					<a href="/zipDomains">Zip Domains</a>
+				</li>
+				<li aria-current={$page.url.pathname.startsWith('/WebSkimmers') ? 'page' : undefined}>
+					<a href="/WebSkimmers">Web Skimmers</a>
+				</li>
+			</ul>
+		</nav>
 	</div>
 </header>
 
 <style>
-	header {
-		display: flex;
-		justify-content: space-between;
-	}
+header {
+	background-color: #E4EFEC;
+	padding: 10px;
+}
 
-	.corner {
-		width: 3em;
-		height: 3em;
-	}
+.header-content {
+	display: flex;
+	flex-direction:flex-start;
+	justify-content: space-between;
+	align-items: left;
+}
 
-	.corner a {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		width: 100%;
-		height: 100%;
-	}
+.page-title {
+	margin: 0;
+	font-size: 3rem;
+	font-weight: bold;
+	position:relative;
+	justify-self:left;
+	margin-right:auto;
+}
 
-	.corner img {
-		width: 2em;
-		height: 2em;
-		object-fit: contain;
-	}
+.page-description {
+	margin: 0;
+	font-size: 1rem;
+	font-weight:lighter;
+	inline-size: 20rem;
+  overflow-wrap: break-word;
+}
 
-	nav {
-		display: flex;
-		justify-content: center;
-		--background: rgba(255, 255, 255, 0.7);
-	}
+.header-left {
+	display: flex;
+	flex-direction: column;
+}
 
-	svg {
-		width: 2em;
-		height: 3em;
-		display: block;
-	}
+.header-right {
+	display: flex;
+}
 
-	path {
-		fill: var(--background);
-	}
+nav ul {
+	list-style: none;
+	padding: 0;
+	margin: 0;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
 
-	ul {
-		position: relative;
-		padding: 0;
-		margin: 0;
-		height: 3em;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		list-style: none;
-		background: var(--background);
-		background-size: contain;
-	}
+nav li {
+	margin: 0 10px;
+}
 
-	li {
-		position: relative;
-		height: 100%;
-	}
+nav li[aria-current='page'] a {
+	background-color: var(--button-color-primary);
+	color: var(--button-color-secondary);
+}
 
-	li[aria-current='page']::before {
-		--size: 6px;
-		content: '';
-		width: 0;
-		height: 0;
-		position: absolute;
-		top: 0;
-		left: calc(50% - var(--size));
-		border: var(--size) solid transparent;
-		border-top: var(--size) solid var(--color-theme-1);
-	}
+nav a {
+	display: inline-block;
+	padding: 10px 20px;
+	border: 1px solid #333;
+	border-radius: 10px;
+	text-decoration: none;
+	color: var(--color-accent);
+	font-weight: 700;
+	font-size: 0.8rem;
+	text-transform: uppercase;
+	letter-spacing: 0.1em;
+	transition: color 0.2s linear;
+}
 
-	nav a {
-		display: flex;
-		height: 100%;
-		align-items: center;
-		padding: 0 0.5rem;
-		color: var(--color-text);
-		font-weight: 700;
-		font-size: 0.8rem;
-		text-transform: uppercase;
-		letter-spacing: 0.1em;
-		text-decoration: none;
-		transition: color 0.2s linear;
-	}
-
-	a:hover {
-		color: var(--color-theme-1);
-	}
+a:hover {
+	color: var(--color-accent);
+}
 </style>
